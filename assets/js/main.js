4ca91c7fae0b75deb50396aafb08c56d343d45d3
Luxibre <leverriernicolas@hotmail.fr>
2024-11-20 21:54:53 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
+import cart from './cart.js';
+import products from './products.js';
 
+// Rendre ces objets disponibles globalement
+window.cart = cart;
+window.products = products;
+
+// Initialisation du panier
+cart.init();
+
+// Configuration Alpine.js si nécessaire
+if (window.Alpine) {
+    window.Alpine.store('cart', cart.items);
+    window.Alpine.store('products', products);
+}
