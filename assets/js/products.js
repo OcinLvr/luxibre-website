875fd64bedddbae265ca59c49e6b354b47c4a020
Luxibre <leverriernicolas@hotmail.fr>
2024-11-21 22:55:13 +0100

    Update products.js

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,3 +1,4 @@
+// products.js
 export default [
     {
         id: 1,
@@ -5,7 +6,7 @@ export default [
         description: "Montre de luxe en acier inoxydable avec cadran en or rose",
         price: 1299.99,
         originalPrice: 1599.99,
-        image: "luxibre-website/assets/images/products/montre.jpg", // Chemin relatif mis à jour
+        image: "assets/images/products/montre.jpg", // Chemin relatif mis à jour
         category: "Accessoires"
     }
 ];
