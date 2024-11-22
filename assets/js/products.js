deae57da1c488e56a98082a3df30ea5e87c209ee
Luxibre <leverriernicolas@hotmail.fr>
2024-11-22 13:03:04 +0100

    Update products.js

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,16 +1,18 @@
-// products.js
-export default [
+// /assets/js/products.js
+export const products = [
     {
         id: 1,
         name: "Montre Élégante",
         description: "Montre de luxe en acier inoxydable avec cadran en or rose",
         price: 1299.99,
         originalPrice: 1599.99,
-        image: "../assets/images/products/montre.jpg",  // Chemin absolu depuis la racine du site
+        image: "/assets/images/products/montre.jpg",  // Chemin absolu
         category: "Accessoires"
     }
 ];
 
-export function calculateDiscount(price, originalPrice) {
+export const calculateDiscount = (price, originalPrice) => {
     return Math.round(((originalPrice - price) / originalPrice) * 100);
-}
+};
+
+export default products;
